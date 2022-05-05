55c01b6c4e1d795ffce6969de412cac5c456718f
andersonbruno02 <andersonbruno02@gmail.com>
2022-05-05 17:49:07 -0300

    Adicionado dependencias para banco de dados e mapeando das classes

--- src/main/java/com/dio/banco/banklineapi/model/Conta.java
+++ src/main/java/com/dio/banco/banklineapi/model/Conta.java
@@ -2,14 +2,20 @@ package com.dio.banco.banklineapi.model;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Embeddable
 public class Conta {
 
+	@Column(name = "numero_conta")
 	private Long numero;
+	@Column(name = "saldo_conta")
 	private BigDecimal saldo;
 
 }
--- src/main/java/com/dio/banco/banklineapi/model/Movimentacao.java
+++ src/main/java/com/dio/banco/banklineapi/model/Movimentacao.java
@@ -3,16 +3,31 @@ package com.dio.banco.banklineapi.model;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "tab_movimentacao")
 public class Movimentacao {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	@Column(name = "data_hora")
 	private LocalDateTime dataHora;
+	
 	private String descricao;
 	private BigDecimal valor;
+	@Enumerated
 	private MovimentacaoTipo tipo;
 }
