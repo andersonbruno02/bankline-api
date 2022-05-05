8106a94fabb543f633922e88373a2e3d7bbb4b2c
andersonbruno02 <andersonbruno02@gmail.com>
2022-05-05 17:03:32 -0300

    subindo base do projeto

---
+++ src/main/java/com/dio/banco/banklineapi/ApiRestDoProjetoBanklineApiApplication.java
@@ -0,0 +1,13 @@
+package com.dio.banco.banklineapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestDoProjetoBanklineApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestDoProjetoBanklineApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dio/banco/banklineapi/ApiRestDoProjetoBanklineApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.banco.banklineapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestDoProjetoBanklineApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
